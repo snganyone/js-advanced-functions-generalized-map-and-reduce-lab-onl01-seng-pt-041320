06e245beb7bdc02681bc945cfee157fe6c068b64
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-15 17:30:06 -0800

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function map(src, call){
     return src.map(call);
 }
 
-function reduce(src, call){
-    return src.reduce(call);
+function reduce(src, call, start){
+    if (start){
+        src.reduce(call, start);
+    } else{
+        src.reduce(call);
+    }
 }
